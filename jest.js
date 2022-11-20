31aad00b522eaf176a09dcba18798340f4114ee6
Guillaume <gui.lebas@gmail.com>
2022-11-20 18:26:07 +0100

    fix(jest) Enable "prefer-comparison-matcher" and "prefer-equality-matcher"

--- jest.js
+++ jest.js
@@ -8,6 +8,8 @@ module.exports = {
         'jest/no-alias-methods': 'warn',
         'jest/no-deprecated-functions': 'warn',
         'jest/no-duplicate-hooks': 'warn',
+        'jest/prefer-comparison-matcher': 'warn',
+        'jest/prefer-equality-matcher': 'warn',
         'jest/prefer-lowercase-title': ['warn', { ignore: ['describe'] }],
         'jest/prefer-to-be': 'warn',
         'jest/prefer-to-contain': 'warn',
