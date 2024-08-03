ab8e98d68d859f98343d13dd4a7da263b49a153c
Guillaume <gui.lebas@gmail.com>
2024-08-03 13:45:16 +0200

    fix(*) Remove rules
    - arrow-body-style
    - block-spacing
    - brace-style
    - constructor-super
    - for-direction
    - generator-star-spacing
    - getter-return
    - logical-assignment-operators
    - max-params
    - multiline-comment-style
    - new-cap
    - new-parens
    - no-async-promise-executor
    - no-class-assign
    - no-compare-neg-zero
    - no-cond-assign
    - no-const-assign
    - no-constant-condition
    - no-control-regex
    - no-delete-var
    - no-dupe-args
    - no-dupe-class-members
    - no-dupe-else-if
    - no-empty
    - no-empty-character-class
    - no-empty-pattern
    - no-ex-assign
    - no-extra-boolean-cast
    - no-extra-semi
    - no-fallthrough
    - no-func-assign
    - no-global-assign
    - no-import-assign
    - no-inner-declarations
    - no-invalid-regexp
    - no-loss-of-precision
    - no-misleading-character-class
    - no-new-symbol
    - no-nonoctal-decimal-escape
    - no-obj-calls
    - no-octal
    - no-prototype-builtins
    - no-redeclare
    - no-regex-spaces
    - no-self-assign
    - no-setter-return
    - no-shadow-restricted-names
    - no-sparse-arrays
    - no-this-before-super
    - no-undef
    - no-unexpected-multiline
    - no-unreachable
    - no-unsafe-finally
    - no-unsafe-negation
    - no-unsafe-optional-chaining
    - no-unused-labels
    - no-useless-backreference
    - no-useless-catch
    - no-useless-escape
    - no-with
    - operator-assignment
    - prefer-const
    - require-yield
    - use-isnan
    - valid-typeof
    - yield-star-spacing
    - jest/no-alias-methods
    - jest/no-conditional-expect
    - jest/no-done-callback
    - jest/no-export
    - jest/no-interpolation-in-snapshots
    - jest/no-jasmine-globals
    - jest/no-mocks-import
    - jest/no-standalone-expect
    - jest/no-test-prefixes
    - jest/prefer-lowercase-title
    - node/no-exports-assign
    - node/no-extraneous-import
    - node/no-extraneous-require
    - node/no-missing-import
    - node/no-missing-require
    - node/no-mixed-requires
    - node/no-unpublished-bin
    - node/no-unpublished-import
    - node/no-unpublished-require
    - node/process-exit-as-throw
    - node/shebang
    - react/jsx-key
    - react/jsx-no-duplicate-props
    - react/jsx-no-target-blank
    - react/jsx-no-undef
    - react/jsx-uses-react
    - react/no-children-prop
    - react/no-danger-with-children
    - react/no-deprecated
    - react/no-direct-mutation-state
    - react/no-find-dom-node
    - react/no-is-mounted
    - react/no-render-return-value
    - react/no-string-refs
    - react/no-unescaped-entities
    - react/prop-types
    - react/react-in-jsx-scope
    - react/require-render-return

--- node.js
+++ node.js
@@ -4,19 +4,8 @@ module.exports = {
     extends: ['@cdoublev/eslint-config'],
     rules: {
         'node/no-deprecated-api': 'warn',
-        'node/no-exports-assign': 'warn',
-        'node/no-extraneous-import': 'warn',
-        'node/no-extraneous-require': 'warn',
-        'node/no-missing-import': 'warn',
-        'node/no-missing-require': 'warn',
-        'node/no-mixed-requires': 'warn',
-        'node/no-unpublished-bin': 'warn',
-        'node/no-unpublished-import': 'warn',
-        'node/no-unpublished-require': 'warn',
         'node/no-unsupported-features/es-builtins': 'warn',
         'node/no-unsupported-features/es-syntax': 'warn',
         'node/no-unsupported-features/node-builtins': 'warn',
-        'node/process-exit-as-throw': 'warn',
-        'node/shebang': 'warn',
     }
 }
